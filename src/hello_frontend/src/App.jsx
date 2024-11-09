0b572f973388a81bb2d2a35f559050f1bdc23328
mxyhi <imxyi@hotmail.com>
2024-11-09 14:45:51 +0800

    feat(hello_backend): add get_submitted_names query and update frontend

--- src/hello_frontend/src/App.jsx
+++ src/hello_frontend/src/App.jsx
@@ -1,29 +1,37 @@
-import { useState } from 'react';
-import { hello_backend } from 'declarations/hello_backend';
+import { useState } from "react";
+import { hello_backend } from "declarations/hello_backend";
 
 function App() {
-  const [greeting, setGreeting] = useState('');
+  const [greeting, setGreeting] = useState("");
+  const [submittedNames, setSubmittedNames] = useState([]);
 
-  function handleSubmit(event) {
+  async function handleSubmit(event) {
     event.preventDefault();
     const name = event.target.elements.name.value;
-    hello_backend.greet(name).then((greeting) => {
-      setGreeting(greeting);
-    });
+    const greeting = await hello_backend.greet(name);
+    setGreeting(greeting);
+    const submittedNames = await hello_backend.get_submitted_names();
+    setSubmittedNames(submittedNames);
     return false;
   }
 
   return (
     <main>
-      <img src="/logo2.svg" alt="DFINITY logo" />
-      <br />
-      <br />
       <form action="#" onSubmit={handleSubmit}>
         <label htmlFor="name">Enter your name: &nbsp;</label>
         <input id="name" alt="Name" type="text" />
         <button type="submit">Click Me!</button>
       </form>
       <section id="greeting">{greeting}</section>
+
+      <section id="submitted-names">
+        <h2>All Greeted Names</h2>
+        <ul>
+          {submittedNames.map((it, i) => {
+            return <li key={i}>{it}</li>;
+          })}
+        </ul>
+      </section>
     </main>
   );
 }
